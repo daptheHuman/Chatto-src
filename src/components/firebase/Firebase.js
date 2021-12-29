2c968a1bbd9e9ac0bf2e185f079cdca833147ebb
Dafa <dafaaqilla72@gmail.com>
2021-12-29 07:40:10 +0700

    make the firebaseConfig to public

--- src/components/firebase/Firebase.js
+++ src/components/firebase/Firebase.js
@@ -19,12 +19,12 @@ import {
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-	appId: process.env.REACT_APP_FIREBASE_APP_ID,
+	apiKey: 'AIzaSyCV8PgKzq1vOtWEJvfJHTvpijkecGP8t1Y',
+	authDomain: 'realtime-chatapp-ed98a.firebaseapp.com',
+	projectId: 'realtime-chatapp-ed98a',
+	storageBucket: 'realtime-chatapp-ed98a.appspot.com',
+	messagingSenderId: '87403505183',
+	appId: '1:87403505183:web:dea42932c6b16f4f54172a',
 };
 
 // Initialize Firebase
